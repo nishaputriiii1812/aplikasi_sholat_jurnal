37008aceafc3c93628b5c247bb87e38542874d91
WINDOWS 11 <nishaputriiii1812@gmail.com>
2026-05-06 12:34:12 +0700

    tbn

---
+++ lib/nisha_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NishaRegister extends StatefulWidget {
+  const NishaRegister({super.key});
+
+  @override
+  State<NishaRegister> createState() => _NishaRegisterState();
+}
+
+class _NishaRegisterState extends State<NishaRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
