1e60b301bf200ad6f2bc4d30640932c11f7784f8
Admin <habibacans98@gmail.com>
2026-05-09 16:00:00 +0700

    biba

---
+++ lib/biba_notif.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BibaNotif extends StatefulWidget {
+  const BibaNotif({super.key});
+
+  @override
+  State<BibaNotif> createState() => _BibaNotifState();
+}
+
+class _BibaNotifState extends State<BibaNotif> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
