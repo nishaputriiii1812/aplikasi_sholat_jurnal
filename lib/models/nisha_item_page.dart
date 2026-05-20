692ddf6fcbe83c3fd9b2d8e26879a2f3a2654d96
WINDOWS 11 <nishaputriiii1812@gmail.com>
2026-05-20 14:48:07 +0700

    tbn

--- lib/models/nisha_item_page.dart
+++ lib/models/nisha_item_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
-class TakimItemPage {
+class NishaItemPage {
   final Widget page;
   final IconData icon;
   final String label;
   final Color color;
 
-  TakimItemPage({
+  NishaItemPage({
     required this.page,
     required this.icon,
     required this.label,
