681acdaab5b7ebe993ac411de6da97bee1d778bb
Admin <habibacans98@gmail.com>
2026-05-20 14:46:43 +0700

    biba

--- lib/service/biba_notif_service.dart
+++ lib/service/biba_notif_service.dart
@@ -3,30 +3,26 @@ import 'package:aplikasi_sholat_jurnal/models/biba_notif_model.dart';
 List<BibaNotifModels> notifications = [
   BibaNotifModels(
     judul: 'siti',
-    notifikasi: 'Vino bf chla',
-    imageUrl:
-        'https://icons.iconarchive.com/icons/aha-soft/free-large-love/256/Wedding-Rings-icon.png',
+    notifikasi: 'menjadi guru',
+    imageUrl: '',
     isRead: false,
   ),
   BibaNotifModels(
     judul: 'carmenita',
-    notifikasi: 'Vino my mine gweh',
-    imageUrl:
-        'https://icons.iconarchive.com/icons/aha-soft/free-large-love/256/Wedding-Rings-icon.png',
+    notifikasi: 'diterima menjadi idol k-pop',
+    imageUrl: '',
     isRead: false,
   ),
   BibaNotifModels(
     judul: 'lyodra',
-    notifikasi: 'Vino nanem sawit',
-    imageUrl:
-        'https://icons.iconarchive.com/icons/aha-soft/free-large-love/256/Wedding-Rings-icon.png',
+    notifikasi: 'diterima snbp',
+    imageUrl: '',
     isRead: false,
   ),
   BibaNotifModels(
     judul: 'zulfa',
-    notifikasi: 'Biba telah menikah dengan jeno',
-    imageUrl:
-        'https://icons.iconarchive.com/icons/aha-soft/free-large-love/256/Wedding-Rings-icon.png',
+    notifikasi: 'menjadi model baju',
+    imageUrl: '',
     isRead: false,
   ),
 ];
